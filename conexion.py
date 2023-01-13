aecad6d92a95407dd30958d4b63702c693aaaff1
Angelozr <118393553+Angelozr@users.noreply.github.com>
2023-01-13 07:25:30 -0500

    Delete conexion.py

--- conexion.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-conexion = mysql.connector.connect(host ='localhost',
-                                            database ='proyecto',
-                                            user ='root', 
-                                            password ='123456')
-                                        
-print(conexion)
-
-                
