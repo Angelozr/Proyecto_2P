3bb9baeaa5d5faba7f2379017010d748753a99d7
Angelozr <118393553+Angelozr@users.noreply.github.com>
2023-01-13 01:21:27 -0500

    .

--- conexion.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-conexion = mysql.connector.connect(user='root', password='mysql123',
-                                    host='localhost',
-                                    database='proyecto',
-                                    port='3306')
-print(conexion)
